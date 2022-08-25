47142bf725b68cfa04bada5366da81efa5281dde
Return <return.work@mail.ru>
2022-08-25 14:32:04 +0300

    add footer

--- js/main.js
+++ js/main.js
@@ -1,8 +1,14 @@
 $(document).ready(function(){
+  $('.intro-slider').slick({
+    arrows: false,
+  });
+
   $('.gallery-slider').slick({
     arrows: false,
     infinite: true,
     fade: true,
-    dots: true
+    dots: true,
+    autoplay: true,
+    autoplaySpeed: 2000,
   });
 });
